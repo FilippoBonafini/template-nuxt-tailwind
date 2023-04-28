f0051e68435152305549c2e18425930f03c5e1f9
FilippoBonafini <filippo.bonafini.99@icloud.com>
2023-04-29 01:54:34 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1 class="text-red-700 text-5xl">
+      SCHERMATA PRINCIPALE
+    </h1>
+    <IntroBody />
+  </div>
+</template>
---
+++ components/Intro/Body.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        Questo body è un componente
+    </div>
+</template>
