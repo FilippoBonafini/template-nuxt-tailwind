0195efba4437f9caebf69cfa69d9457cee108da1
FilippoBonafini <filippo.bonafini.99@icloud.com>
2023-04-29 19:26:59 +0200

    MODIFICATO TEMPLATE

--- app.vue
+++ app.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="h-screen bg-slate-500">
-    <NuxtLayout>
+  <NuxtLayout>
 
-      <Main />
+    <Main />
 
-    </NuxtLayout>
-  </div>
+  </NuxtLayout>
 </template>
--- components/Main.vue
+++ components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="h-screen flex items-center justify-center">
         QUESTO E' IL CORPO DELLA PAGINA
     </div>
 </template>
